59048ffe8cb79afa27b474fa2b5e0879eb560273
Allysson <allyssoncarlos.ac21@gmail.com>
2025-04-25 13:49:56 -0300

    terceiro commit

---
+++ project/local_settings.py
@@ -0,0 +1,19 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'yO_>l1BybyqJ%^wJdO2>mM4&p#sS^j%3p_&<[57q(=vwIR#hZTdK4/X2u{0'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# ALLOWED_HOSTS = ['34.59.173.212']
+
+# Config para postgresql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'NOME_DA_BASE_DE_DADOS',
+#         'USER': 'USUARIO_DA_BASE_DE_DADOS',
+#         'PASSWORD': 'SENHA_DA_BASE_DE_DADOS',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
